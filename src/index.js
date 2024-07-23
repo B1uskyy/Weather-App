ebf28e3f9ccd8fa451338860380fb84a3414790d
Mats <mats.kvanvik@hotmail.com>
2024-07-23 14:52:20 +0200

    updated so user search for city to view weather

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
+import "./style.css"
+import displayCityName from "./displayCityName";
 
 const API_KEY = "DEP8ML4Y2WDD8V75JFZ8VSRHX"
 const URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 
+let locationInput = document.querySelector(".locationInput");
+let submit = document.querySelector(".submitbtn");
 
 async function getWeatherFromLocation(location) {
-        const response = await fetch(URL + location + "?key=" + API_KEY, {mode: 'cors'});
+        const response = await fetch(URL + location + "/?unitGroup=metric&key=" + API_KEY, {mode: 'cors'});
         const responseData = await response.json();
 
-        console.log(responseData.description);
+        displayCityName(responseData.resolvedAddress);
+
+        console.log(responseData);
 }
 
+submit.addEventListener("click", () => {
+        getWeatherFromLocation(locationInput.value)
+})
 
-getWeatherFromLocation("Oslo");
+
+
